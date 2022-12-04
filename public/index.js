3b87578b69f9a8f1be07cf0421052571b2192752
gabriel-johnson <johnsga2@oregonstate.edu>
2022-12-04 15:14:05 -0800

    scroll work :)

--- public/index.js
+++ public/index.js
@@ -3,9 +3,11 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     
   var header = document.querySelector("header")
-  header.classList.toggle("sticky", window.scrollY >0)
-  var nav_list = document.querySelector("header")
-  nav_list.classList.toggle("sticky_nav", window.scrollY >0)
+  header.classList.toggle("sticky", window.scrollY >100)
+  var nav_list = document.querySelector("nav")
+  nav_list.classList.toggle("sticky_nav", window.scrollY >100)
+  var content = document.querySelector("main")
+  content.classList.toggle("sticky_content", window.scrollY >100)
 
 
 }
